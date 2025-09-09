ad816306ffc787b42b0b7c8d23d5a1a01181069d
samk <202000734@est.umss.edu>
2025-09-09 00:19:48 -0400

    Enpoint Area, hourmeters, typeChange and workOrder finished

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const hourmeterRoutes = require("./routes/hourmeter");
-
+const workOrderSparePartsRoutes = require("./routes/workOrderSpareParts");
+const workOrderTechnicians = require("./routes/workOrderTechnicians")
+const typeChangeMaintenance = require("./routes/typeChangeMaintenance")
+const areas = require("./routes/areas")
 const app = express();
 
 // Middlewares
@@ -10,5 +13,9 @@ app.use(express.json());
 
 // Rutas
 app.use("/hourmeters", hourmeterRoutes);
+app.use("/workOrderSpareParts", workOrderSparePartsRoutes);
+app.use("/workOrderTechnicians", workOrderTechnicians);
+app.use("/typeChangeMaintenance", typeChangeMaintenance);
+app.use("/area", areas);
 
 module.exports = app;
